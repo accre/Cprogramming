432b7ff6b6333bc18b94e9411335c60f01bdb075
accre <will@accre.vanderbilt.edu>
2015-02-05 11:04:38 -0600

    Adding new examples

--- Makefile
+++ Makefile
@@ -1,12 +1,22 @@
 CC=/usr/local/gcc/latest/bin/gcc
 CFLAGS=-Wall
 
-all: hello_world variable_types pass_command_line_options
+all: hello_world variable_types pass_command_line_options \
+     functions flow_control loops
 
 hello_world: hello_world.c
 	$(CC) -o $@ $< $(CFLAGS)
 
 variable_types: variable_types.c
+	$(CC) -o $@ $< 
+
+flow_control: flow_control.c
+	$(CC) -o $@ $< $(CFLAGS) 
+
+loops: loops.c
+	$(CC) -o $@ $< $(CFLAGS)
+
+functions: functions.c
 	$(CC) -o $@ $< $(CFLAGS)
 
 pass_command_line_options: pass_command_line_options.c
@@ -18,3 +28,6 @@ clean:
 	rm hello_world
 	rm variable_types
 	rm pass_command_line_options
+	rm loops
+	rm functions
+	rm flow_control
--- functions.c
+++ functions.c
@@ -1,7 +1,56 @@
 #include <stdio.h>
 
 
+float mySimpleFuncByValue(int iloc,float xloc)
+{
+
+   iloc += 101;
+   xloc *= 14.6;
+
+   float yloc = (float)iloc * xloc;
+   
+   return yloc;   
+
+}
+
+float mySimpleFuncByReference(int * iPtr,float * xPtr)
+{
+
+   // dereference the pointer and modify the value it points to
+   *iPtr += 101;
+   *xPtr *= 14.6;
+
+   // make copies to the values that each pointer points to
+   int iloc = *iPtr;
+   float xloc = *xPtr;
+
+   float yloc = (float)iloc * xloc;
+   
+   return yloc;   
+
+}
+
+
 int main()
 {
+
+   int i = 22;
+   float x = 234.23;
+
+   // i and x are both passed by value,
+   // meaning a copy of each of these variables
+   // is passed to mySimpleFunc
+   float y = mySimpleFuncByValue(i,x);
+   printf("i: %d x: %10.5f y: %10.5f\n",i,x,y);
+   // Now i and x are both being passed by reference
+   // (Notice the & symbol)
+   // This means that the actual memory location of 
+   // i and x are being passed, so any modifications
+   // to the values i and x within the function will
+   // propogate to main() 
+   y = mySimpleFuncByReference(&i,&x);
+   printf("i: %d x: %10.5f y: %10.5f\n",i,x,y);
+
+   return 0;
 
 }
